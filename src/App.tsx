a641d18a24b328c5841c5fc9ce2a72c096a9c051
nvsriram <nuranisri01@gmail.com>
2023-09-04 04:29:19 +0400

    Added opacity and shape controls to GUI

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,34 @@ import { OrbitControls, Stats } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useControls } from "leva";
 import "./App.css";
-import Cube from "./components/Cube";
+import FractalCube from "./components/FractalCube";
 import Lights from "./components/Lights";
 import Screens from "./components/Screens";
+import { ShapeOptions } from "./components/types";
 
 const App = () => {
-  const controls = useControls("Fractal Cube", {
+  const {
+    shape,
+    scale,
+    dimension,
+    iterations,
+    color,
+    opacity,
+    showText,
+    showAxes,
+    showGrid,
+    showStats,
+  } = useControls("Imaginary Cube Fractal Controls", {
+    shape: {
+      options: ShapeOptions,
+    },
     scale: {
       value: 0.9,
       min: 0.1,
       max: 1,
       step: 0.1,
     },
-    n: {
+    dimension: {
       value: 2,
       min: 1,
       max: 10,
@@ -29,6 +44,12 @@ const App = () => {
     color: {
       value: "cyan",
     },
+    opacity: {
+      value: 0.4,
+      min: 0,
+      max: 1,
+      step: 0.1,
+    },
     showText: {
       value: true,
     },
@@ -42,22 +63,25 @@ const App = () => {
       value: true,
     },
   });
+
   return (
     <>
-      <Canvas camera={{ position: [0, 0, controls.n * 2] }} shadows>
-        <Lights distance={controls.n * 2} />
-        <Cube
-          initialScale={controls.scale}
-          size={controls.n}
-          iterations={controls.iterations}
-          color={controls.color}
-          showText={controls.showText}
+      <Canvas camera={{ position: [0, 0, dimension * 2] }} shadows>
+        <Lights distance={dimension * 2} />
+        <FractalCube
+          shape={shape}
+          initialScale={scale}
+          size={dimension}
+          iterations={iterations}
+          color={color}
+          opacity={opacity}
+          showText={showText}
         />
-        <Screens scale={controls.n * 2} />
-        {controls.showStats && <Stats />}
+        <Screens scale={dimension * 2} />
+        {showStats && <Stats />}
         <OrbitControls target={[0, 0, 0]} />
-        <axesHelper args={[controls.n]} visible={controls.showAxes} />
-        <gridHelper visible={controls.showGrid} />
+        <axesHelper args={[dimension]} visible={showAxes} />
+        <gridHelper visible={showGrid} />
       </Canvas>
     </>
   );
