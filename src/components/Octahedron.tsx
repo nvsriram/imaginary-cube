3a368719285371c97c9a19c1bcf50db6d184195a
nvsriram <nuranisri01@gmail.com>
2023-09-07 20:48:29 +0400

    Added edges to Octahedron

--- src/components/Octahedron.tsx
+++ src/components/Octahedron.tsx
@@ -1,4 +1,4 @@
-import { Octahedron as OctahedronPrimitive } from "@react-three/drei";
+import { Edges, Octahedron as OctahedronPrimitive } from "@react-three/drei";
 import { useControls } from "leva";
 import { useMemo } from "react";
 import { IShape } from "../types";
@@ -9,6 +9,7 @@ const Octahedron = ({
   size,
   iterations,
   material,
+  showEdges,
 }: IShape) => {
   const scale = useMemo(
     () => size / Math.pow(size, Math.pow(2, iterations)),
@@ -32,7 +33,14 @@ const Octahedron = ({
       material={material}
       castShadow
       receiveShadow
-    />
+    >
+      <Edges
+        scale={scale}
+        threshold={15}
+        material={material}
+        visible={showEdges}
+      />
+    </OctahedronPrimitive>
   );
 };
 
