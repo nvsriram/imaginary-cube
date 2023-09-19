6f626f9f62cde1059bd15c441ae8df38b43f29ee
nvsriram <nuranisri01@gmail.com>
2023-09-20 02:07:31 +0400

    Added logo to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,13 @@
+import Logo from "../assets/logo.svg";
+
 const Navbar = () => {
   return (
     <nav className="container w-full mx-auto py-5 flex flex-col items-center sm:justify-end">
       <a
-        className="text-3xl sm:text-2xl lg:text-2xl flex font-semibold sm:mr-auto pl-2 text-folderTextColor outline-none"
+        className="text-3xl sm:text-2xl flex flex-col sm:flex-row gap-3 sm:gap-2 items-center font-semibold sm:mr-auto pl-2 text-folderTextColor outline-none"
         href={`/`}
       >
+        <img src={Logo} className="h-15 sm:h-11" />
         <h1>Imaginary Cube Visualizer</h1>
       </a>
     </nav>
