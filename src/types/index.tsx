e0828fdc606891261e16e8225f2a42cd2d54b02a
nvsriram <nuranisri01@gmail.com>
2023-10-18 00:21:29 +0400

    Added control options

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -7,17 +7,26 @@ import Octahedron from "../components/Octahedron";
 import Tetrahedron from "../components/Tetrahedron";
 import { convertToBetaShape } from "../utils";
 
+// Base types
 type CubeGrid = boolean[][][];
 type CubePositions = Vector3[];
 type LatinSquare = number[][];
 
+// Context types
 type ObjContextType = {
   obj: Group;
-
   geometry: BufferGeometry | null;
 };
 
-type Shape = FC<IShape>;
+// Control types
+const ControlOptions = [
+  { keys: ["Click"], action: "start/stop rotation" },
+  { keys: ["Hover"], action: "slow down rotation" },
+  { keys: ["p"], action: "open/close pause menu" },
+  { action: "Use the side panel to control the fractal cube" },
+];
+
+// Shape component types
 interface IShape {
   pos: THREE.Vector3;
   initialScale: number;
@@ -28,15 +37,14 @@ interface IShape {
   opacity: number;
   showEdges: boolean;
 }
-
-const ShapeMap = new Map<string, FC<IShape>>([
+type Shape = FC<IShape>;
+const ShapeMap = new Map<string, Shape>([
   ["cube", Cube],
   ["tetrahedron", Tetrahedron],
   ["cuboctahedron", Cuboctahedron],
   ["hexagonal bipyramid", HexagonalBipyramid],
   ["octahedron", Octahedron],
 ]);
-
 const ShapeMapKeys = Array.from(ShapeMap.keys());
 const DefaultShapeMapKeys = ["cube", "tetrahedron"];
 const BetaShapeMapKeys = convertToBetaShape(
@@ -44,7 +52,7 @@ const BetaShapeMapKeys = convertToBetaShape(
 );
 const AllShapeMapKeys = [...DefaultShapeMapKeys, ...BetaShapeMapKeys];
 
-export { AllShapeMapKeys, DefaultShapeMapKeys, ShapeMap };
+export { AllShapeMapKeys, ControlOptions, DefaultShapeMapKeys, ShapeMap };
 export type {
   CubeGrid,
   CubePositions,
