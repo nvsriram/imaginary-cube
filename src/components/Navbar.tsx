4b0eca138f5c4404cce0e2315f189f90d8c83dd4
nvsriram <nuranisri01@gmail.com>
2023-10-21 03:09:26 +0400

    Updated Navbar z-index

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,12 @@ import Logo from "../assets/logo.svg";
 
 const Navbar = () => {
   return (
-    <nav className="container mx-auto flex w-full flex-col items-center pt-5 sm:justify-end">
+    <nav className="container z-[10002] mx-auto flex w-full flex-col items-center pt-5 sm:justify-end">
       <a
         className="flex flex-col items-center gap-3 pl-2 text-3xl font-semibold text-folderTextColor outline-none sm:mr-auto sm:flex-row sm:gap-2 sm:text-2xl"
         href={`/`}
       >
-        <img src={Logo} className="h-20 sm:h-11" />
+        <img src={Logo} className=" h-20 sm:h-11" />
         <h1>Imaginary Cube Visualizer</h1>
       </a>
     </nav>
