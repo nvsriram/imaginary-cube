4db1f70efcdd899366db4a1ca6da06cbc59ee2c6
nvsriram <nuranisri01@gmail.com>
2023-10-26 01:43:02 +0400

    Added Waiting dot animation

---
+++ src/components/WaitingAnimation.tsx
@@ -0,0 +1,11 @@
+const WaitingAnimation = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="mx-0.5 inline-block h-0.5 w-0.5 animate-[jumpingAnimation_0.8s_0.2s_ease-in_infinite] bg-highlight3" />
+      <div className="mx-0.5 inline-block h-0.5 w-0.5 animate-[jumpingAnimation_0.8s_0.4s_ease-in_infinite] bg-highlight3" />
+      <div className="mx-0.5 inline-block h-0.5 w-0.5 animate-[jumpingAnimation_0.8s_0.6s_ease-in_infinite] bg-highlight3" />
+    </div>
+  );
+};
+
+export default WaitingAnimation;
