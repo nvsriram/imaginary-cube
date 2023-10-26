5986ece08b2eef045d6b714927fbadb717cba19b
nvsriram <nuranisri01@gmail.com>
2023-10-27 02:02:22 +0400

    Added Kbd component

---
+++ src/components/Kbd.tsx
@@ -0,0 +1,13 @@
+interface IKbd {
+  text: string;
+}
+
+const Kbd = ({ text }: IKbd) => {
+  return (
+    <span className="inline-flex min-w-[24px] items-center justify-center rounded-sm border border-b-4 border-transparent border-b-highlight2 bg-highlight3 p-1 text-sm text-elevation2">
+      {text}
+    </span>
+  );
+};
+
+export default Kbd;
