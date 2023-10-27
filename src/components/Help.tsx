31788343e3893b46fcb83f03671c3194a069edc8
nvsriram <nuranisri01@gmail.com>
2023-10-27 17:35:30 +0400

    Updated Help to use ControlOptions

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -1,41 +1,18 @@
 import { ControlOptions } from "../types";
+import Kbd from "./Kbd";
 
 const Help = () => {
   return (
-    <div className="flex flex-col text-highlight1">
-      {ControlOptions.map(({ keys, action }) => (
-        <div>
+    <div className="flex flex-col items-center justify-center gap-2 text-highlight1">
+      {ControlOptions.map(({ keys, action }, idx) => (
+        <div
+          key={idx}
+          className="inline-flex h-full w-full flex-row items-center justify-between"
+        >
           <span>{action}</span>
-          <div>
-            {keys?.map((key, idx) => (
-              <span
-                key={idx}
-                className="inline-flex min-w-[24px] items-center justify-center rounded-sm border border-b-4 border-transparent border-b-highlight2 bg-highlight3 p-1 text-sm text-elevation2"
-              >
-                {key}
-              </span>
-            ))}
-          </div>
+          <div>{keys?.map((key, idx) => <Kbd text={key} key={idx} />)}</div>
         </div>
       ))}
-      {/* <div>
-        <span className="text-accent3">Hover</span> to slow down rotation
-      </div>
-      <div>
-        <span className="text-accent2">Click</span> to start/stop rotation
-      </div>
-      <div>Use side panel to control fractal cube</div>
-      <div>
-        <span className="text-accent1">reset scene</span> resets rotation and
-        camera
-      </div>
-      <div>
-        <span className="inline-flex min-w-[24px] items-center justify-center rounded-sm border border-b-4 border-transparent border-b-highlight2 bg-highlight3 p-1 text-sm text-elevation2">
-          p
-        </span>{" "}
-        to open pause menu
-      </div>
-    </div> */}
     </div>
   );
 };
