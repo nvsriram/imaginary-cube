0361c9f7b71cf4448a996585d7079e0be423de99
nvsriram <nuranisri01@gmail.com>
2023-11-01 01:10:59 +0400

    Added Shruggie component

---
+++ src/components/Shruggie.tsx
@@ -0,0 +1,13 @@
+const Shruggie = () => {
+  return (
+    <>
+      <span className="text-vivid1">¯\_</span>
+      <span className="text-[#00ffff]">(</span>
+      <span className="text-folderTextColor">ツ</span>
+      <span className="text-[#00ffff]">)</span>
+      <span className="text-vivid1">_/¯</span>
+    </>
+  );
+};
+
+export default Shruggie;
