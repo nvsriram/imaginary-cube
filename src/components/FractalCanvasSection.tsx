b4993433c298a652f9fef7c6e57ec2c42b8ef2cd
nvsriram <nuranisri01@gmail.com>
2023-11-03 00:50:15 +0400

    Added FractalCanvasSection

---
+++ src/components/FractalCanvasSection.tsx
@@ -0,0 +1,11 @@
+import FractalCanvas from "./FractalCanvas";
+
+const FractalCanvasSection = () => {
+  return (
+    <section className="flex h-full w-full grow flex-col py-3 sm:py-5">
+      <FractalCanvas />
+    </section>
+  );
+};
+
+export default FractalCanvasSection;
