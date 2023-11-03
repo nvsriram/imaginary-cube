97d5b3b277f45c95f1e5b1d24b7bbb4ea61c38c3
nvsriram <nuranisri01@gmail.com>
2023-11-04 01:03:52 +0400

    Added IntroSection

---
+++ src/components/IntroSection.tsx
@@ -0,0 +1,23 @@
+const IntroSection = () => {
+  return (
+    <section className="flex h-full w-full flex-col items-center justify-center gap-3 text-center font-normal text-folderWidgetColor sm:text-start">
+      <p className="w-full">
+        an{" "}
+        <a
+          href="https://www.i.h.kyoto-u.ac.jp/users/tsuiki/imaginarycube-e.html"
+          className="text-vivid1"
+        >
+          imaginary cube
+        </a>{" "}
+        is a 3D object that has square silhouettes in the three orthogonal
+        directions.
+      </p>
+      <p className="w-full">
+        mess around with these potential imaginary cubes to witness the
+        interesting shadows they form! 🎉
+      </p>
+    </section>
+  );
+};
+
+export default IntroSection;
