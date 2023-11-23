afdc3fb6ad2ba65c3f0e54a72dd4b7a212aacede
nvsriram <nuranisri01@gmail.com>
2023-11-24 01:10:35 +0400

    Refactored kbd to ui

--- src/components/ui/Kbd.tsx
+++ src/components/ui/Kbd.tsx
@@ -1,13 +1,11 @@
-interface IKbd {
+export interface IKbd {
   text: string;
 }
 
-const Kbd = ({ text }: IKbd) => {
+export const Kbd = ({ text }: IKbd) => {
   return (
     <span className="inline-flex min-w-[24px] items-center justify-center rounded-sm border border-b-4 border-transparent border-b-highlight2 bg-highlight3 p-1 text-sm text-elevation2">
       {text}
     </span>
   );
 };
-
-export default Kbd;
