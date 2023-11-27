b64e13313330c090862fd8e8240b771f75282708
nvsriram <nuranisri01@gmail.com>
2023-11-28 00:50:45 +0400

    Update selection colors

--- src/components/ui/Shruggie.tsx
+++ src/components/ui/Shruggie.tsx
@@ -1,11 +1,11 @@
 export const Shruggie = () => {
   return (
     <>
-      <span className="text-vivid1">¯\_</span>
-      <span className="text-[#00ffff]">(</span>
+      <span className="text-vivid1 selection:!text-vivid1">¯\_</span>
+      <span className="text-[#00ffff] selection:!text-[#00ffff]">(</span>
       <span className="text-folderTextColor">ツ</span>
-      <span className="text-[#00ffff]">)</span>
-      <span className="text-vivid1">_/¯</span>
+      <span className="text-[#00ffff] selection:!text-[#00ffff]">)</span>
+      <span className="text-vivid1 selection:!text-vivid1">_/¯</span>
     </>
   );
 };
