6b725cc2055455f60d4a9442ded3589de8e1a01d
nvsriram <nuranisri01@gmail.com>
2023-11-28 02:06:51 +0400

    Added shruggie easter egg

--- src/components/ui/Shruggie.tsx
+++ src/components/ui/Shruggie.tsx
@@ -1,10 +1,20 @@
+import { useSearchParams } from "react-router-dom";
+
 export const Shruggie = () => {
+  const [searchParams] = useSearchParams();
+
+  const color = searchParams.get("color") ?? "#00ffff";
+
   return (
     <>
       <span className="text-vivid1 selection:!text-vivid1">¯\_</span>
-      <span className="text-[#00ffff] selection:!text-[#00ffff]">(</span>
+      <span className="selection:!text-inherit" style={{ color }}>
+        (
+      </span>
       <span className="text-folderTextColor">ツ</span>
-      <span className="text-[#00ffff] selection:!text-[#00ffff]">)</span>
+      <span className="selection:!text-inherit" style={{ color }}>
+        )
+      </span>
       <span className="text-vivid1 selection:!text-vivid1">_/¯</span>
     </>
   );
