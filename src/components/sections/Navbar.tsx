a8923d8fade8f2beb1afe0688fb458ab0ea88597
nvsriram <nuranisri01@gmail.com>
2023-11-28 00:46:24 +0400

    Replace link with span

--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -10,13 +10,10 @@ export const Navbar = () => {
         paused ? "z-[10002]" : "z-auto"
       } mx-auto flex w-full flex-col items-center pt-5 sm:justify-end`}
     >
-      <a
-        className="flex flex-col items-center gap-3 pl-2 text-3xl font-semibold text-folderTextColor outline-none sm:mr-auto sm:flex-row sm:gap-2 sm:text-2xl"
-        href="#"
-      >
-        <img className=" h-20 sm:h-11" src={Logo} alt="logo" />
+      <span className="flex flex-col items-center gap-3 pl-2 text-3xl font-semibold text-folderTextColor outline-none sm:mr-auto sm:flex-row sm:gap-2 sm:text-2xl">
+        <img alt="logo" className=" h-20 sm:h-11" src={Logo} />
         <h1>imaginary cube visualizer</h1>
-      </a>
+      </span>
     </nav>
   );
 };
