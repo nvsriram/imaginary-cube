8574751d673c796c08228ab30700a43ce3199620
nvsriram <nuranisri01@gmail.com>
2023-11-28 02:09:11 +0400

    Update animation name

--- src/components/ui/WaitingAnimation.tsx
+++ src/components/ui/WaitingAnimation.tsx
@@ -1,9 +1,9 @@
 export const WaitingAnimation = () => {
   return (
     <div className="flex items-center justify-center">
-      <div className="mx-0.5 inline-block h-0.5 w-0.5 animate-[jumpingAnimation_0.8s_0.2s_ease-in_infinite] bg-highlight3" />
-      <div className="mx-0.5 inline-block h-0.5 w-0.5 animate-[jumpingAnimation_0.8s_0.4s_ease-in_infinite] bg-highlight3" />
-      <div className="mx-0.5 inline-block h-0.5 w-0.5 animate-[jumpingAnimation_0.8s_0.6s_ease-in_infinite] bg-highlight3" />
+      <div className="mx-0.5 inline-block h-0.5 w-0.5 animate-[waitingAnimation_0.8s_0.2s_ease-in_infinite] bg-highlight3" />
+      <div className="mx-0.5 inline-block h-0.5 w-0.5 animate-[waitingAnimation_0.8s_0.4s_ease-in_infinite] bg-highlight3" />
+      <div className="mx-0.5 inline-block h-0.5 w-0.5 animate-[waitingAnimation_0.8s_0.6s_ease-in_infinite] bg-highlight3" />
     </div>
   );
 };
